b167d53b00b0f29df98da01ef63424d72f8bf85e
Lahari-nagaraj <158993362+Lahari-nagaraj@users.noreply.github.com>
2025-02-19 23:13:22 +0530

    generate trip from

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import CreateTrip from './create-trip/index.jsx'
 import Header from './components/custom/Header' 
 import { Toaster } from "@/components/ui/sonner";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 
 const router=createBrowserRouter([
   {
@@ -22,8 +24,10 @@ const router=createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
-    <Toaster />
-    <RouterProvider router={router} />
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <Header />
+      <Toaster />
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
